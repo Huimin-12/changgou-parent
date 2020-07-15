b7a5094f0c945717c9ee9b5c1cfde93d6c4b1cf1
huimin <2237501874@qq.com>
2020-07-15 22:27:18 +0800

    今日完成品牌接口

--- changgou_service/changgou_service_goods/src/main/java/com/changgou/service/goods/controller/BrandController.java
+++ changgou_service/changgou_service_goods/src/main/java/com/changgou/service/goods/controller/BrandController.java
@@ -1,73 +1,59 @@
 package com.changgou.service.goods.controller;
 
-import com.changgou.common.pojo.PageResult;
 import com.changgou.common.pojo.Result;
 import com.changgou.common.pojo.StatusCode;
 import com.changgou.goods.pojo.Brand;
 import com.changgou.service.goods.service.BrandService;
-import com.github.pagehelper.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
-@RequestMapping("/brand")
 @RestController
+@RequestMapping("/brand")
 public class BrandController {
 
     @Autowired
     private BrandService brandService;
     //查询所有
-    @GetMapping
-    public Result<List<Brand>> findList(){
+    @GetMapping("/findAll")
+    public Result<List<Brand>> findAll(){
         List<Brand> brandList = brandService.findList();
-        return new Result<>(true, StatusCode.OK,"查询成功",brandList);
-    }
-    //根据id查询
-    @GetMapping("/{id}")
-    public Result<Brand> findById(@PathVariable("id") Integer id){
-        Brand brand = brandService.findById(id);
-        return new Result<>(true,StatusCode.OK,"查询成功",brand);
-    }
-    //添加信息
-    @PostMapping
-    public Result add(@RequestBody Brand brand){
-        brandService.add(brand);
-        return new Result(true,StatusCode.OK,"添加成功");
+
+        return new Result(true, StatusCode.OK,"查询所有成功",brandList);
     }
 
-    @PutMapping("/{id}")
-    public Result update(@PathVariable("id") Integer id,@RequestBody Brand brand){
-        brand.setId(id);
-        brandService.update(brand);
-        return new Result(true,StatusCode.OK,"修改成功");
+    //根据id进行品牌查询
+    @GetMapping("/findById/{id}")
+    public Result findById(@PathVariable("id") Integer id){
+
+        try {
+            Brand brand = brandService.findById(id);
+            return new Result(true,StatusCode.OK,"查询成功",brand);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false,StatusCode.ERROR,"查询失败");
+        }
     }
 
-    @DeleteMapping("/{id}")
-    public Result delById(@PathVariable("id") Integer id){
+
+    //根据id进行品牌的删除
+    @DeleteMapping("/delete/{id}")
+    public Result deleteId(@PathVariable("id") Integer id){
         brandService.delById(id);
         return new Result(true,StatusCode.OK,"删除成功");
     }
 
-    @GetMapping("/search")
-    public Result<List<Brand>> search(@RequestParam Map searchMap){
-        List<Brand> list = brandService.list(searchMap);
-        return new Result<>(true,StatusCode.OK,"查询成功",list);
+    //添加品牌信息
+    @PostMapping("/add")
+    public Result add(@RequestBody Brand brand){
+        brandService.add(brand);
+        return new Result(true,StatusCode.OK,"添加成功");
     }
-
-    @GetMapping("/search/{page}/{size}")
-    public Result findPage(@PathVariable("page")int page,@PathVariable("size") int size){
-        Page<Brand> pageInfo = brandService.findPage(page, size);
-        PageResult pageResult = new PageResult(pageInfo.getTotal(),pageInfo.getResult());
-        return new Result(true,StatusCode.OK,"查询成功",pageResult);
-    }
-
-    @GetMapping("/searchPage/{page}/{size}")
-    public Result findPage(@RequestParam Map searchMap,@PathVariable("page")int page,@PathVariable("size") int size){
-        int i=1/0;
-        Page pageInfo = brandService.findPage(searchMap, page, size);
-        PageResult pageResult = new PageResult(pageInfo.getTotal(),pageInfo.getResult());
-        return new Result(true,StatusCode.OK,"查询成功",pageResult);
+    //修改品牌信息
+    @PutMapping()
+    public Result update(@RequestBody Brand brand){
+        brandService.update(brand);
+        return new Result(true,StatusCode.OK,"修改成功");
     }
 }
